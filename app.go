47143d6f52df6319b86c7b7b3e62b72740847a80
jamesoneill997 <jamesoneill997@gmail.com>
2020-11-13 00:47:26 +0000

    added draw function

--- app.go
+++ app.go
@@ -1,5 +1,29 @@
 package main
 
-/*
+import (
+	"fmt"
+	"strings"
+)
 
- */
+/*
+snake
+controls
+food
+border
+movement
+hit logic
+*/
+
+func DrawBox() {
+	for i := 0; i <= 20; i++ {
+		if i == 0 || i == 20 {
+			fmt.Println(strings.Repeat("-", 31))
+		} else {
+			fmt.Println("|", strings.Repeat(" ", 27), "|")
+		}
+	}
+}
+
+func main() {
+	DrawBox()
+}
