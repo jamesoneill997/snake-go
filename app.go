537a9362b6e34be1aff114ed036b5e19feec93fb
jamesoneill997 <jamesoneill997@gmail.com>
2020-11-13 01:03:29 +0000

    updated drawBoard function

--- app.go
+++ app.go
@@ -12,18 +12,32 @@ food
 border
 movement
 hit logic
+score
 */
 
-func DrawBox() {
-	for i := 0; i <= 20; i++ {
-		if i == 0 || i == 20 {
-			fmt.Println(strings.Repeat("-", 31))
+//Snake is the player struct
+type Snake struct {
+	length string
+	alive  bool
+	speed  float32
+}
+
+//DrawBox function will draw the gameboard
+func DrawBox(w int, h int, snakePos [2]int, foodPos [2]int) {
+	for i := 0; i <= h; i++ {
+		if i == 0 || i == h {
+			fmt.Println(strings.Repeat("-", w))
 		} else {
-			fmt.Println("|", strings.Repeat(" ", 27), "|")
+			fmt.Println("|", strings.Repeat(" ", w-4), "|")
 		}
 	}
 }
 
 func main() {
-	DrawBox()
+	boardSize := [2]int{124, 80} //h,w - Ratio of 31:20
+	snakePos := [2]int{0, 0}
+	foodPos := [2]int{10, 10}
+
+	DrawBox(boardSize[0], boardSize[1], snakePos, foodPos)
+
 }
